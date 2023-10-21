0cab7a49453c65b81ccc62d74f1de0763ffd746f
Lütfi Eren Esinler <erenesinler23@gmail.com>
2023-10-21 14:08:14 +0300

    Add files via upload

---
+++ Week 8 - HTML, CSS, JavaScript/Problem Set 8/homepage/script.js
@@ -0,0 +1,5 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    var messageElement = document.getElementById("message");
+    messageElement.innerHTML = "Hello from JavaScript!";
+    messageElement.style.color = "green";
+  });
